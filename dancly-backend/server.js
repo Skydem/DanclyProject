7f291f358585de34a14179e457f066e57599f9e0
Piotr <piotrzych98@gmail.com>
2022-06-02 21:14:26 +0200

    add new endpoints like login or signup to the app

--- dancly-backend/server.js
+++ dancly-backend/server.js
@@ -1,11 +1,10 @@
 import express from 'express'
 import { MongoClient } from "mongodb";
 import Cors from "cors"
-
-import dbCards from './dbCards.js';
+import bcrypt from "bcrypt";
+const jwt = require('jsonwebtoken');
 
 // App Config
-
 const app = express();
 const port = process.env.PORT || 8001
 
@@ -17,50 +16,114 @@ app.use(Cors());
 
 // DB Config
 
+// Important variables
+const client = new MongoClient(connection_url)
+const dbname = client.db('danclydb')
+const users = dbname.collection('users')
+const user = await users.findOne({email});
+
 // API Endpoints
 app.get('/', (req, res) => res.status(200).send('Hello'));
 
 app.get('/login', (req, res) => res.status(200));
 
-app.post('/signup', (req, res) => {
-    const client = new MongoClient(connection_url)
-})
+app.post('login', async (req, res) => {
+    const {email, password} = req.body
 
-app.get('/users', async (req, res) => {
-    const client = new MongoClient(connection_url)
+    try {
+        await client.connect();
+        const correctPass = await bcrypt.compare(password, user.hashed_password);
+
+        if(user && correctPass) {
+            const token = jwt.sign(user, email, {
+                expiresIn: 720,
+            });
+            res.status(200).json({token, userId: user.user_id})
+        }
+        res.status(400).send('Niepoprawne dane logowania.');
+
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+app.post('/signup', async (req, res) => {
+    const {email, password} = req.body
+
+    const generatedUserId = uuidv1();
+    const hashedPass = await bcrypt.hash(password, 10);
 
     try {
         await client.connect()
-        const dbname = client.db('danclydb')
-        const users = dbname.collection('users')
 
-        const returnedUsers = await users.find().toArray()
-        res.send(returnedUsers)
+        if(user) {
+            return res.status(409).send("Użytkownik z podanym mailem istnieje. Proszę się zalogować.")
+        }
+
+        const sanitizedEmail = email.toLowerCase()
+        const data = {
+            user_id: generatedUserId,
+            email: sanitizedEmail,
+            hashed_password: hashedPass
+        }
+        const insertedUser = await users.insertOne(data)
+
+        const token = jwt.sign(insertedUser, sanitizedEmail, {
+            expiresIn: 720,
+        });
+
+        res.status(201).json({token, userId: generatedUserId});
+
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+app.get('/user', async (req,res) => {
+    const userId = req.params.userId;
+
+    try {
+        await client.connect();
+
+        const query = { user_id: userId };
+        const user = await users.findOne(query);
+
+        res.send(user);
     } finally {
-        await client.close()
+        await client.close();
     }
 })
 
-app.post('/dancly/cards', (req, res) => {
-    const dbCard = req.body;
-    dbCards.create(dbCard, (err, data) => {
-        if (err) {
-            res.status(500).send(err);
-        } else {
-            res.status(201).send(data);
-        }
-    })
-})
+app.put('/user', async (req, res) => {
+    const formData = req.body.formData;
 
-app.get('/dancly/cards', (req, res) => {
-    dbCards.create((err, data) => {
-        if (err) {
-            res.status(500).send(err);
-        } else {
-            res.status(200).send(data);
+    try {
+        await client.connect();
+
+        const query = {user_id: formData.user_id};
+        const updateInfo = {
+            $set: {
+                first_name: formData.first_name,
+                last_name: formData.last_name,
+                birth_day: formData.birth_day,
+                birth_month: formData.birth_month,
+                birth_year: formData.birth_year,
+                gender_identity: formData.gender_identity,
+                gender_interest: formData.gender_interest,
+                url: formData.url,
+                about: formData.about,
+                matches: formData.matches
+            },
         }
-    })
-})
+
+        const insertedUser = await  users.updateOne(query, updateInfo);
+
+        res.json(insertedUser);
+
+    } finally {
+        await client.close();
+    }
+});
 
 /* Listener */
 app.listen(port, () => console.log('Listening on localhost: ' + port));
