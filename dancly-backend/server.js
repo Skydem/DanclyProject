fe669cb5f86445ae9c032108f55826f58b4092e8
Piotr <piotrzych98@gmail.com>
2022-06-02 22:29:36 +0200

    add messages endpoints and cleanup code

--- dancly-backend/server.js
+++ dancly-backend/server.js
@@ -2,24 +2,24 @@ import express from 'express'
 import { MongoClient } from "mongodb";
 import Cors from "cors"
 import bcrypt from "bcrypt";
+
 const jwt = require('jsonwebtoken');
 
 // App Config
 const app = express();
-const port = process.env.PORT || 8001
+const port = process.env.PORT || 8001;
 
-const connection_url = "mongodb+srv://admin:admin1@cluster0.v31sc.mongodb.net/Cluster0?retryWrites=true&w=majority"
+// DB Config
+const connection_url = "mongodb+srv://admin:admin1@cluster0.v31sc.mongodb.net/Cluster0?retryWrites=true&w=majority";
 
 // Middlewares
 app.use(express.json());
 app.use(Cors());
 
-// DB Config
-
 // Important variables
-const client = new MongoClient(connection_url)
-const dbname = client.db('danclydb')
-const users = dbname.collection('users')
+const client = new MongoClient(connection_url);
+const database = client.db('danclydb');
+const users = database.collection('users');
 const user = await users.findOne({email});
 
 // API Endpoints
@@ -28,7 +28,7 @@ app.get('/', (req, res) => res.status(200).send('Hello'));
 app.get('/login', (req, res) => res.status(200));
 
 app.post('login', async (req, res) => {
-    const {email, password} = req.body
+    const {email, password} = req.body;
 
     try {
         await client.connect();
@@ -36,7 +36,7 @@ app.post('login', async (req, res) => {
 
         if(user && correctPass) {
             const token = jwt.sign(user, email, {
-                expiresIn: 720,
+                expiresIn: 900,
             });
             res.status(200).json({token, userId: user.user_id})
         }
@@ -69,7 +69,7 @@ app.post('/signup', async (req, res) => {
         const insertedUser = await users.insertOne(data)
 
         const token = jwt.sign(insertedUser, sanitizedEmail, {
-            expiresIn: 720,
+            expiresIn: 900,
         });
 
         res.status(201).json({token, userId: generatedUserId});
@@ -92,7 +92,21 @@ app.get('/user', async (req,res) => {
     } finally {
         await client.close();
     }
-})
+});
+
+app.get('/gendered-users', async (req, res) => {
+    const gender = req.query.gender; // w Dashboardzie trzeba będzie pobrać wartość gender
+
+    try {
+        await client.connect();
+        const query = {gender_identity: {$eq: gender}};
+        const returnedUsers = await users.find(query).toArray();
+
+        res.send(returnedUsers);
+    } finally {
+        await client.close();
+    }
+});
 
 app.put('/user', async (req, res) => {
     const formData = req.body.formData;
@@ -119,6 +133,77 @@ app.put('/user', async (req, res) => {
         const insertedUser = await  users.updateOne(query, updateInfo);
 
         res.json(insertedUser);
+
+    } finally {
+        await client.close();
+    }
+});
+
+app.put('/add-match', async (req, res) => {
+    const { userId, matchedUserId } = req.body;
+
+    try {
+        await client.connect();
+        const query = { user_id: userId };
+        const updateDoc = {
+            $push: { matches: { user_id: matchedUserId}},
+        }
+        res.send(await users.updateOne(query, updateDoc));
+    } finally {
+        await client.close();
+    }
+});
+
+app.get('/users', async (req, res) => {
+    const userIds = JSON.parse(req.query.userIds); // Dodać z dashboard.js
+
+    try {
+        await client.connect();
+        const pipeline =
+            [
+                {
+                    '$match': {
+                        'user_id': {
+                            '$in': userIds
+                        }
+                    }
+                }
+            ];
+
+        const foundUsers = await users.aggregate(pipeline).toArray();
+        res.send(foundUsers);
+
+    } finally {
+        await client.close();
+    }
+});
+
+app.get('/messages', async (req, res) => {
+    const messages = database.collection('messages');
+    const { userId, correspondingUserId} = req.query;
+    try {
+        await client.connect();
+        const query = {
+            from_userId: userId, to_userId: correspondingUserId
+        }
+        const foundMessages = await messages.find(query).toArray();
+
+        res.send(foundMessages);
+    } finally {
+        await client.close();
+    }
+
+});
+
+app.post('/message', async (req, res) => {
+    const message = req.body.message;
+
+    try {
+        await client.connect();
+        const messages = database.collection('messages');
+        const sentMessage = await messages.insertOne(message);
+
+        res.send(sentMessage);
 
     } finally {
         await client.close();
